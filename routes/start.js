85e3cce858d90b87e94e136f4eaecc4099ec2f4c
Wicoco <147388824+Wicoco@users.noreply.github.com>
2024-04-09 23:51:30 +0200

    Nodejs Harry

--- routes/start.js
+++ routes/start.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const UsersController = require("../controllers/UsersController");
-const AuthentificationController = require("../controllers/AuthentificationController");
 const { authenticateToken } = require("../middlewares/Auth");
+
+//USERS
 
 const router = express.Router();
 
-router.get("/users", UsersController.index); // GET /users
-router.post("/users", UsersController.store); // POST /users
-router.get("/users/:id", UsersController.show);
+router.get("/users", UsersController.index); //SEE ALL
+router.post("/users", UsersController.store); //CREATE
+router.get("/users/:id", UsersController.show); //SEE SOMEONE
+router.put("/users/:id", UsersController.update); //UPDATE
+router.delete("/users/:id", UsersController.delete); //DELETE
+
+//POSTS
+
+const PostsController = require("../controllers/PostsController");
+
+router.get("/posts", PostsController.index); //SEE ALL
+router.post("/posts", PostsController.store); //CREATE
+router.get("/posts/:id", PostsController.show); //SEE SOMEONE
+router.put("/posts/:id", PostsController.update); //UPDATE
+router.delete("/posts/:id", PostsController.delete); //DELETE
+
+//Authentification
+
+const AuthentificationController = require("../controllers/AuthentificationController");
+
 router.post("/login", AuthentificationController.login);
 router.get(
   "/getMyProfile",
   authenticateToken,
   AuthentificationController.getMyProfile
 );
+
+module.exports = router;
 
 module.exports = router;
